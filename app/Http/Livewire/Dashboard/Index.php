1bf3b8490279e15b576a1857bd291a907dc49c2c
arjundex <arjun.dexbytes@gmail.com>
2023-04-05 15:25:32 +0530

    dashboard content removed

--- app/Http/Livewire/Dashboard/Index.php
+++ app/Http/Livewire/Dashboard/Index.php
@@ -5,7 +5,6 @@ namespace App\Http\Livewire\Dashboard;
 use Carbon\Carbon;
 use App\Models\User;
 use Livewire\Component;
-use App\Models\Order\Order;
 use Illuminate\Support\Facades\DB;
 use App\Traits\GlobalTrait;
 class Index extends Component
@@ -44,102 +43,7 @@ class Index extends Component
                                 $query->where('name' , '=' ,  'Customer');
                                 })->where('created_at','>=', $this->dateBeforeSeven)->count();      
         }
-        
-        //Total orders of 7 days
-        if(auth()->user()->hasRole('Provider')){
-            $this->totalOrders = Order::where('store_id', $this->getStoreId())->where('created_at','>=', $this->dateBeforeSeven)->count();
-            $this->totalCompletedOrders = Order::where('order_status', 'completed')->where('store_id', $this->getStoreId())->where('created_at','>=', $this->dateBeforeSeven)->count();
-        }else{
-            $this->totalOrders = Order::where('created_at','>=', $this->dateBeforeSeven)->count();
-            $this->totalCompletedOrders = Order::where('order_status', 'completed')->where('created_at','>=', $this->dateBeforeSeven)->count();
-        }
-        //Total sales of 7 days           
-        if(auth()->user()->hasRole('Provider')){
-                $this->sales = Order::where('store_id', $this->getStoreId())->where([
-                                ['created_at','>=', $this->dateBeforeSeven],
-                                ['order_status', '=' ,'completed']
-                                ])->sum('total_amount');
-        }else{
-            $this->sales = Order::where([
-                    ['created_at','>=', $this->dateBeforeSeven],
-                    ['order_status', '=' ,'completed']
-                    ])->sum('total_amount');
-        }
-
-       
-            //Total orders of 7 days	
-            if(auth()->user()->hasRole('Provider')){	
-                $orders = Order::where([['created_at','>=', $this->dateBeforeSeven]])
-                        ->where('store_id', $this->getStoreId())
-                        ->whereIn('order_status', $this->orderStatus)
-                        ->select('order_status', DB::raw('count(id) as total'))
-                        ->groupBy('order_status')->get();
-            }else{
-                $orders = Order::where([['created_at','>=', $this->dateBeforeSeven]])
-                        ->whereIn('order_status',  $this->orderStatus)
-                        ->select('order_status', DB::raw('count(id) as total'))
-                        ->groupBy('order_status')->get();
-            }
-            $orderCounts = [];
-            foreach($orders as $row) {
-                $orderCounts[$row->order_status] = (int) $row->total;
-            }
- 
-            $this->data['label'] = $this->orderStatus;
-            foreach ($this->orderStatus as $oKey => $oValue) {
-                $this->data['data'][] = (int) array_key_exists($oValue, $orderCounts) ? $orderCounts[$oValue] : 0;
-            }
-            // Total orders of 7 months
-            $orderRevenues = [];                           
-                switch (env('DB_CONNECTION')) {
-                case 'pgsql':
-                    $orderRevenues_query = order::whereDate("created_at",">=",  Carbon::now()->subMonths(6)->firstOfMonth())
-                    ->select(DB::raw('count(id)
- as total'),DB::raw("DATE_PART('year', created_at::date) as year"),DB::raw("DATE_PART('month',created_at::date) as months"),DB::raw("TO_CHAR(created_at::date,'Mon') as month"),DB::raw('COUNT("order_status") filter (where order_status = '."'completed'".') AS completed'))
-                    ->whereIn('order_status',['completed', 'pending', 'cancelled', 'refund'])
-                    ->groupby('year','months','month');
-                break;
-                case 'mysql':
-                    $orderRevenues_query = order::whereDate("created_at",">=",  Carbon::now()->subMonths(6)->firstOfMonth())
-                    ->whereIn('order_status',['completed', 'pending', 'cancelled', 'refund'])
-                    ->select(DB::raw('count(id)  as total'), DB::raw('DATE_FORMAT(created_at, "%b") as month'), DB::raw('COUNT((SELECT order_status="completed" where order_status="completed")) as completed'),DB::raw('YEAR(created_at) year, MONTH(created_at) months'))->groupby('year','months');
-                break;
-
-                default:
-                return;
-                break;
-                }
-                if(auth()->user()->hasRole('Provider')){	
-                    $orderRevenues_query->where('store_id', $this->getStoreId());
-                }
-                $orderRevenues = $orderRevenues_query->get();
-            
-            $validateMonths = [];
-            for ($i = 6; $i >= 0; $i--) {
-                $month = Carbon::today()->subMonth($i);
-                array_push($validateMonths,
-                    $month->shortMonthName
-                );
-            }
-
-			$monthData = [];
-			foreach($orderRevenues as $orderRevenue) {
-				$monthData[$orderRevenue['month']] = $orderRevenue;
-			}
-			foreach ($validateMonths as $value) {
-				    $this->data['months'][] = $value;
-				if(isset($monthData[$value])) {
-                    $this->data['total'][] = $monthData[$value]->total;
-                    $this->data['completed'][] = $monthData[$value]->completed;
-				}else{
-					$this->data['total'][] = 0;
-					$this->data['completed'][] = 0;
-				}
-			}
-            
-
-    } 
-
+    }
     public function render()
     {
         return view('livewire.dashboard.home');
--- resources/views/livewire/dashboard/home.blade.php
+++ resources/views/livewire/dashboard/home.blade.php
@@ -1,7 +1,7 @@
 @section('page_title') Dashboard @endsection
 <div class="container-fluid py-4">
    <div class="row">
-        <div class="col-sm-3">
+        <div class="col-sm-6">
             <div class="card">
                 <div class="card-body p-3 position-relative">
                     <div class="row">
@@ -16,7 +16,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-3 mt-sm-0 mt-4">
+        <div class="col-sm-6 mt-sm-0 mt-4">
             <div class="card">
                 <div class="card-body p-3 position-relative">
                    @if(auth()->user()->hasRole('Provider'))
@@ -55,114 +55,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-3 mt-sm-0 mt-4">
-            <div class="card">
-                <div class="card-body p-3 position-relative">
-                    <div class="row">
-                        <div class="col-7 text-start">
-                            <p class="text-sm mb-1 text-capitalize font-weight-bold">Total Order</p>
-                            <h5 class="font-weight-bolder mb-0">
-                                {{ $totalOrders }}
-                            </h5>
-                        </div>
-                        <div class="col-5">
-                            <div class="dropdown text-end">
-                                <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers3" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <span class="ms-lg-n6 text-xs text-secondary">{{ $dateBeforeSeven->format('d M') }} - {{ $todayDate->format('d M') }}</span>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-3 mt-sm-0 mt-4">
-            <div class="card">
-                <div class="card-body p-3 position-relative">
-                    <div class="row">
-                        <div class="col-7 text-start">
-                            <p class="text-sm mb-1 text-capitalize font-weight-bold">Sales</p>
-                            <h5 class="font-weight-bolder mb-0 me-lg-n1">
-                                {{ \Utils::ConvertPrice($sales) }}
-                            </h5>
-                        </div>
-                        <div class="col-5">
-                            <div class="dropdown text-end">
-                                <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers3" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <span class="ms-lg-n6 text-xs text-secondary">{{ $dateBeforeSeven->format('d M') }} - {{ $todayDate->format('d M') }}</span>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-    <div class="row mt-4">
-        <div class="col-lg-4 col-sm-6">
-            <div class="card h-100">
-                <div class="card-header pb-0 p-3">
-                    <div class="d-flex justify-content-between">
-                        <h6 class="mb-0">Orders</h6>
-                    </div>
-                </div>
-                <div class="card-body pb-0 p-3 mt-4">
-                    <div class="row">
-                        <div class="col-7 text-start">
-                            <div class="chart">
-                                <canvas id="chart-pie" class="chart-canvas" height="200"></canvas>
-                            </div>
-                        </div>
-                        <div class="col-5 my-auto">
-                            <span class="badge badge-md badge-dot me-4 d-block text-start">
-                                <i class="bg-info"></i>
-                                <span class="text-dark text-xs">Completed</span>
-                            </span>
-                            <span class="badge badge-md badge-dot me-4 d-block text-start">
-                                <i class="bg-primary"></i>
-                                <span class="text-dark text-xs">Pending</span>
-                            </span>
-                            <span class="badge badge-md badge-dot me-4 d-block text-start">
-                                <i class="bg-dark"></i>
-                                <span class="text-dark text-xs">Cancelled</span>
-                            </span>
-                            <span class="badge badge-md badge-dot me-4 d-block text-start">
-                                <i class="bg-secondary"></i>
-                                <span class="text-dark text-xs">Refund</span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-8 col-sm-6 mt-sm-0 mt-4">
-            <div class="card">
-                <div class="card-header pb-0 p-3">
-                    <div class="d-flex justify-content-between">
-                        <h6 class="mb-0">Revenue</h6>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <span class="badge badge-md badge-dot me-4">
-                            <i class="bg-primary"></i>
-                            <span class="text-dark text-xs">Total</span>
-                        </span>
-                        <span class="badge badge-md badge-dot me-4">
-                            <i class="bg-dark"></i>
-                            <span class="text-dark text-xs">Completed</span>
-                        </span>
-                    </div>
-                </div>
-                <div class="card-body p-3">
-                    <div class="chart">
-                        <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
 </div>
 
 @push('js') 
